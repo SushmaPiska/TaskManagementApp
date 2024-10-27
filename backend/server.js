3afc33bb3162ab749ceae4b2201a82619a40beb9
Sushma Piska <piskasushma@gmail.com>
2024-10-27 11:26:08 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express"; //type:module
 // const  express=require("express") //type: commonjs
 import dotenv from "dotenv";
@@ -7,19 +8,27 @@ import mongoose from "mongoose";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cors from "cors";
 
-dotenv.config();
+dotenv.config({path:'./backend/.env'});
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const __dirname=path.resolve()
 
 app.use(express.json()); 
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials:true,
 }));
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')));
+
 app.use("/api/auth",userRouter);
 app.use("/api/auth",taskRouter);
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,'frontend','dist','index.html'));
+})
 
 app.listen(PORT, () => {
   connectToMongoDB();
