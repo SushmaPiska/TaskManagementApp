bb8e3c44a294fcaf17412708f62d8da586d6e3bc
Sushma Piska <piskasushma@gmail.com>
2024-10-27 15:00:55 +0530

    deploy

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -5,7 +5,11 @@ dotenv.config()
 
 const connectToMongoDB=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI)
+        const mongoURI=process.env.MONGO_DB_URI  || 'mongodb://127.0.0.1:27017/taskManageApp'
+        await mongoose.connect(process.env.MONGO_DB_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         console.log("connected to MongoDB")
     } catch (error) {
         console.log("Error connecting to mongoDB",error.message)
