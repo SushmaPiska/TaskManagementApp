65150356f967842f92a2e15de3b84c70ab43893c
Sushma Piska <piskasushma@gmail.com>
2024-10-27 12:26:22 +0530

    cors  updated

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const __dirname=path.resolve()
 
 app.use(express.json()); 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin:[ 'http://localhost:5173','https://taskmanagementapp-4.onrender.com'],
   credentials:true,
 }));
 
