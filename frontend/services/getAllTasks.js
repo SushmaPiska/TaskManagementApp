320da2da886bce48bde4738a0768e85ce9454bff
Sushma Piska <piskasushma@gmail.com>
2024-11-02 08:12:48 +0530

    viteBaseUrl updated

--- frontend/services/getAllTasks.js
+++ frontend/services/getAllTasks.js
@@ -1,21 +1,42 @@
 import axios from "axios";
-import { addTokenToHeader } from "../helper/addToken";
 
 export const getAllTasksByType = (taskType) => {
-  const tasks = axios.post("http://localhost:8000/api/auth/getAllTasksByType", {
+  const tasks = axios.post(`${import.meta.env.VITE_BASE_URL}/api/auth/getAllTasksByType`, {
     taskType: taskType,
   });
   return tasks;
 };
 
-export const getTasksByPriority = (priority ) => {
+export const getTasksByPriority = (priority) => {
   const tasks = axios.post(
-    "http://localhost:8000/api/auth/getTasksByPriority",
+    `${import.meta.env.VITE_BASE_URL}/api/auth/getTasksByPriority`,
     {
       priority: priority,
     }
   );
   return tasks;
-
-  // const tasks=axios.get(`${import.meta.env.VITE_BASE_URL}/api/auth/getTasksByPriority`,{priority:priority})
 };
+
+
+
+// import axios from "axios";
+// import { addTokenToHeader } from "../helper/addToken";
+
+// export const getAllTasksByType = (taskType) => {
+//   const tasks = axios.post("http://localhost:8000/api/auth/getAllTasksByType", {
+//     taskType: taskType,
+//   });
+//   return tasks;
+// };
+
+// export const getTasksByPriority = (priority ) => {
+//   const tasks = axios.post(
+//     "http://localhost:8000/api/auth/getTasksByPriority",
+//     {
+//       priority: priority,
+//     }
+//   );
+//   return tasks;
+
+//   // const tasks=axios.get(`${import.meta.env.VITE_BASE_URL}/api/auth/getTasksByPriority`,{priority:priority})
+// };
